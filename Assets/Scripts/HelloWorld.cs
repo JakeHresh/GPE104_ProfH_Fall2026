4cb8a7b5c2366fe7d42835adf070f269051b107c
Jacob Hreshchyshyn <jakeyhresh123@gmail.com>
2026-09-15 11:08:11 -0700

    Updated HelloWorld script

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -5,12 +5,14 @@ public class HelloWorld : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello World!");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log("Hello from Update!");
+
+        Debug.Log("This is a new addition in Update!");
     }
 }
