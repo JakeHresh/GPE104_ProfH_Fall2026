d8ef7839ae233fe9cf356b7bb5ab723d75f11186
Jacob Hreshchyshyn <jakeyhresh123@gmail.com>
2026-09-17 10:51:33 -0700

    Added new Development Script

---
+++ Assets/Scripts/TestWithDev.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TestWithDev : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello from Dev Branch");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
